71d7b0f5279e6e3840de08cda289c52a1d60a6a8
JORAM <joramkamugisha2@gmail.com>
2026-03-31 05:45:35 +0300

    initial commit

--- app/src/main/java/com/ndejje/momocalc/MainActivity.kt
+++ app/src/main/java/com/ndejje/momocalc/MainActivity.kt
@@ -10,6 +10,8 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
@@ -30,7 +32,7 @@ class MainActivity : ComponentActivity() {
 
 
 @Composable
-fun BrkenInput() {
+fun InternalStateInput() {
    var amount = ""
 
     TextField(
@@ -46,6 +48,6 @@ fun BrkenInput() {
 @Composable
 fun Preview() {
     MomoCalculatorAppTheme {
-        BrkenInput()
+        InternalStateInput()
     }
 }
