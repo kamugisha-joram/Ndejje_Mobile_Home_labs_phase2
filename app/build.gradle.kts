1c735aa6019424e9340ca611aaa1c9d93b6db9c5
JORAM <joramkamugisha2@gmail.com>
2026-04-06 16:03:19 +0300

    initial commit

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,14 +6,12 @@ plugins {
 
 android {
     namespace = "com.ndejje.momocalc"
-    compileSdk {
-        version = release(36)
-    }
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.ndejje.momocalc"
         minSdk = 24
-        targetSdk = 36
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
